88d15a73fa47fe882c79ed50c200978c59c5dce5
inanmike <mikeinan@hotmail.com>
2025-04-19 05:09:39 +0300

    tabs-services-comp

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,35 @@
+import { View, Text, Image, ScrollView } from 'react-native'
+import React from 'react'
+import { Link, useRouter } from 'expo-router'
+import { images } from '@/constants/images'
+import { icons } from '@/constants/icons'
+import SearchBar from '@/components/SearchBar'
+
+const Index = () => {
+
+  const router = useRouter()
+
+  return (
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className='absolute w-full z-0'/>
+      <ScrollView className='flex-1 px-5'
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{minHeight:'100%', paddingBottom: 10}}
+      >
+        <Image 
+          source={icons.logo} 
+          className='w-12 h-10 mt-20 mb-5 mx-auto'
+        />
+
+        <View>
+          <SearchBar 
+            onPress={() => router.push('/search')}
+            placeholder='Search for a movie..'
+          />
+        </View>
+      </ScrollView>
+    </View>
+  )
+}
+
+export default Index
